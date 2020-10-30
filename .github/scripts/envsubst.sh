49f1021b4725ad53c74d2f3f754d0f3e1ee5dca0
Dominic Watson <watson.dominic@gmail.com>
2020-10-30 17:42:45 +0000

    Delete aold shell files

--- .github/scripts/envsubst.sh
+++
@@ -1,9 +0,0 @@
-for FILE in $FILES; do
-	if [[ -n $PATTERNS ]] ; then
-		envsubst "${PATTERNS}" < $FILE > /tmp/envsubst || exit 1
-	else
-		envsubst < $FILE > /tmp/envsubst || exit 1
-	fi
-
-	mv /tmp/envsubst $FILE || exit 1
-done
